3de69f9751ce55bf40fe49dd937e1e5bfe081a66
Nuggu Venkata Bhavani Shankar <shankarnuggu@gmail.com>
2026-05-27 17:26:49 +0530

    all done

--- config/settings.py
+++ config/settings.py
@@ -81,10 +81,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'breath_esg',
+        'ENGINE': 'your url',
+        'NAME': 'data base name',
         'USER': 'postgres',
-        'PASSWORD': 'Sk1911',
+        'PASSWORD': 'your password',
         'HOST': 'localhost',
         'PORT': '5432',
     }
