04c27d62136c0fe1d7e2e86b233055a68ab09000
Nuggu Venkata Bhavani Shankar <shankarnuggu@gmail.com>
2026-05-27 16:24:57 +0530

    Fixed render postgres database

--- config/settings.py
+++ config/settings.py
@@ -79,15 +79,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'breath_esg',
-        'USER': 'postgres',
-        'PASSWORD': 'Sk1911',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 
